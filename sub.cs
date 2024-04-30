059508b471e52563ccc91f2c032d1901668c27d6
nayanak123-github <168564569+nayanak123-github@users.noreply.github.com>
2024-04-30 22:16:31 +0530

    Update sub.cs

--- sub.cs
+++ sub.cs
@@ -1,1 +1,12 @@
+diff --git a/sub.cs b/sub.cs
+index c5d3a91..6e655d3 100644
+--- a/sub.cs
++++ b/sub.cs
+@@ -6, 5 +6 @@ indes 96052da..2bb2e01 100644
 
+public sub(int a , int b)
+{
+- int c = a - b;
++ int d = a - b;
+}
+  
